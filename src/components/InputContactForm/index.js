c54fd73e03d7b9b85708987442e863ff554fa085
NanaBanana14 <nananabananayaya@gmail.com>
2024-08-09 03:11:02 +0700

    validation done

--- src/components/InputContactForm/index.js
+++ src/components/InputContactForm/index.js
@@ -8,19 +8,28 @@ const InputContactForm = (props) => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
 
-  // TODO: Uncomment baris kode di bawah untuk membuat regex yang akan membantu memvalidasi format nomor telepon dan email
+  // Regex untuk memvalidasi format nomor telepon dan email
   const regexPhoneNumber = /^[0-9]*$/;
   const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-  // TODO:
-  // 1. Buat sebuah fungsi yang akan memvalidasi apakah format dari nomor telepon dan email yang dimasukkan sudah benar atau belum
-  // 2. Jika format nomor telepon salah, maka tampilkan sebuah alert dengan isi pesan "Nomor telepon hanya dapat berupa angka."
-  // 3. Jika format email salah, maka tampilkan sebuah alert dengan isi pesan "Format email tidak sesuai."
-  // 4. Jika format nomor telepon dan email sudah benar, maka lanjutkan proses untuk membuat kontak baru atau meng-update kontak
+  // Fungsi untuk memvalidasi input nomor telepon dan email
+  const validateInput = () => {
+    if (!regexPhoneNumber.test(phoneNumber)) {
+      alert("Nomor telepon hanya dapat berupa angka.");
+      return false;
+    }
+    if (!regexEmail.test(email)) {
+      alert("Format email tidak sesuai.");
+      return false;
+    }
+    return true;
+  };
 
   const { handleGetContacts, selectedContact } = props;
 
   const handleSubmit = async () => {
+    if (!validateInput()) return;
+
     if (id) {
       await updateContactInfo({
         id,
